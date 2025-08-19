47113e9cd3f7ebb3e7ee8e17d9f2c2a747a6b3d7
Aditya Kasarla <134717535+A-K-0@users.noreply.github.com>
2025-08-20 00:36:42 +0530

    Time: 75 ms (18.53%) | Memory: 20.8 MB (22.41%) - LeetSync

--- Arrays/2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ Arrays/2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -4,20 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        j = 0
         zeros = 0
         output = 0
 
-        # while i and j:
-
-        #     if nums[j] == 0:
-        #         zeros += 1
-        #         j += 1
-
-        #     elif nums[j] != 0:
-        #         output += (zeros(zeros + 1))/2
-        #         zeros = 0
 
         for i in range(len(nums)):
             if nums[i] == 0:
