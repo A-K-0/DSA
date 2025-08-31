819f02f8616ca93ba4d03308f12a3bac3f620a97
Aditya Kasarla <134717535+A-K-0@users.noreply.github.com>
2025-09-01 01:06:17 +0530

    Time: 39 ms (15.73%) | Memory: 12.5 MB (16.70%) - LeetSync

---
+++ Recursion/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        EvenSum = 0
+        OddSum = 0
+
+        if n == 1:
+            return 1
+
+        for i in range(1,n*2+1):
+            if i%2 == 0:
+                EvenSum += i
+
+            else:
+                OddSum += i
+
+        def GCD(EvenSum, OddSum):
+            
+            OddSum = EvenSum % OddSum
+            temp = EvenSum
+            EvenSum = OddSum
+
+            if OddSum == 0:
+                return temp
+ 
+            return GCD(EvenSum,OddSum)
+
+        return GCD(EvenSum,OddSum)
+
+
+
+
+
+        
