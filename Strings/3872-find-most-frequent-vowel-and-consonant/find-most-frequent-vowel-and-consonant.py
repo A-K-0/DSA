46541a94e87ac9adf3ef2d5cc2a93b9ad6558957
Aditya Kasarla <134717535+A-K-0@users.noreply.github.com>
2025-09-13 23:18:44 +0530

    Time: 322 ms (5.36%) | Memory: 12.5 MB (53.86%) - LeetSync

---
+++ Strings/3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def maxFreqSum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freq  = [0] * 26
+
+        MaxVow,MaxCons = 0,0
+
+        for char in s:
+
+            x = ord(char) - ord('a')
+
+            if char in 'aeiou':
+
+                freq[x] += 1
+                MaxVow = max(MaxVow,freq[x])
+
+            else:
+
+                freq[x] += 1
+                MaxCons = max(MaxCons,freq[x])
+
+            print(MaxVow,MaxCons)
+
+            print(chr(97), chr(97) in 'aeiou')
+
+        return MaxVow + MaxCons
